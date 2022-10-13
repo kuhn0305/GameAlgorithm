9f88614523da1726dae75162fb3494954eb2ea92
HyunWoo Kang <kuhn0305@gmail.com>
2022-10-13 16:50:46 +0900

    221013 BinaryTree 미로 생성 알고리즘

--- Algorithm/Algorithm/Board.cs
+++ Algorithm/Algorithm/Board.cs
@@ -126,19 +126,27 @@ namespace Algorithm
 
         public void Initialize(int size)
         {
+            // 홀수 크기의 맵 생성
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
+            // 길을 막아버리는 작업
             for (int y = 0; y < size; y++)
             {
                 for (int x = 0; x < size; x++)
                 {
-                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
                 }
             }
+
+            //BinaryTree 미로 생성 알고리즘
+            GenerateByBinaryTree();
         }
 
         public void Render()
@@ -168,6 +176,41 @@ namespace Algorithm
                     return ConsoleColor.Red;
                 default:
                     return ConsoleColor.Green;
+            }
+        }
+
+        private void GenerateByBinaryTree()
+        {
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    // size - 2는 벽 바로 앞의 길을 의미함
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                        _tile[y, x + 1] = TileType.Empty;
+                    else
+                        _tile[y + 1, x] = TileType.Empty;
+                }
             }
         }
     }
