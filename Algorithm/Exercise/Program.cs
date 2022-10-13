439c9baf1cabc6d6a581df6892a42d5ddb0347b0
HyunWoo Kang <kuhn0305@gmail.com>
2022-10-14 02:00:51 +0900

    그래프 생성 2가지 방법
    1. 2차원 배열
    2. 리스트 배열

---
+++ Algorithm/Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercise
+{
+    class Program
+    {
+        int[,] adj = new int[6, 6]
+        {
+            { 0, 1, 0, 1, 0, 0 },
+            { 0, 0, 1, 1, 0, 0 },
+            { 0, 1, 0, 0, 0, 0 },
+            { 1, 1, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 1 },
+            { 0, 0, 0, 0, 1, 0 }
+        };
+
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int> { 1, 3 },
+            new List<int> { 0, 2, 3 },
+            new List<int> { 1 },
+            new List<int> { 0, 1, 4 },
+            new List<int> { 3, 5 },
+            new List<int> { 4 },
+        };
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
